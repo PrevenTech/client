1f831638be28bd5db53a65c761b838170812d641
Jacobo Soffer <sofferjacob@gmail.com>
2022-02-05 22:14:45 -0600

    .

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import workmanager
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,12 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    WorkmanagerPlugin.setPluginRegistrantCallback { registry in
+        // registry in this case is the FlutterEngine that is created in Workmanager's performFetchWithCompletionHandler
+        // This will make other plugins available during a background fetch
+        GeneratedPluginRegistrant.register(with: registry)
+    }
+    UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(60*15))
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
